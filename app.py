57566c86ae5c201beeb66a8f2608a53aad6854a2
Herimiaina ANDRIA-NTOANINA <herimiaina@yahoo.ca>
2020-12-18 00:05:13 +0300

    type: fix subject: Fixed Babel not recognizing LC_TIME

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ def format_datetime(value, format='medium'):
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
       format="EE MM, dd, y h:mma"
-  print('expecting some', date, ' the date')
-  return babel.dates.format_datetime(date, format)
+  # babel cannot find the local using LC_TIME
+  return babel.dates.format_datetime(date, format, locale='en')
 
 app.jinja_env.filters['datetime'] = format_datetime
 
